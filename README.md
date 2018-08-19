3d1bac0fb8174e0fae605ca4d4a10a479a4af62b
blouns <blouns@microsoft.com>
2018-08-18 22:06:03 -0700

    World is born

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 "# wism" 
+Go away.
---
+++ cwism/wism/World.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wism
+{
+    public class World
+    {
+        private enum mapObject : int
+        {
+            None = 0,
+            Mountain = 1,
+            Meadow = 2,
+            Castle = 3
+        }
+
+        private static int[,] map =
+        {
+            { 1, 1, 1, 1, 1 },
+            { 1, 2, 2, 2, 1 },
+            { 1, 2, 3, 2, 1 },
+            { 1, 2, 2, 2, 1 },
+            { 1, 1, 1, 1, 1 }
+        };
+    }
+}
