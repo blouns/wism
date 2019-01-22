f913ee12d3c1c592928f82f22a6783e33b6ff028
blouns <blouns@outlook.com>
2019-01-21 21:00:56 -0800

    Adding WISM Unity Files back w/ proper GIT ignore template
    Re-adding without all the metadata and package files.

---
+++ WISM/Assets/Scripts/Tiles/TilemapManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TilemapManager : MonoBehaviour
+{
+    static bool hasRun = false;
+
+    void Update()
+    {
+        if (!hasRun)
+        {
+            hasRun = true;
+        }
+        else
+        {
+            return;
+        }
+            
+        Tilemap tilemap = GetComponent<Tilemap>();
+
+        BoundsInt bounds = tilemap.cellBounds;
+        TileBase[] allTiles = tilemap.GetTilesBlock(bounds);
+
+        for (int x = 0; x < bounds.size.x; x++) {
+            for (int y = 0; y < bounds.size.y; y++) {
+                TileBase tile = allTiles[x + y * bounds.size.x];
+                if (tile != null) {
+                    Debug.Log("x:" + x + " y:" + y + " tile:" + tile.name);
+                } else {
+                    Debug.Log("x:" + x + " y:" + y + " tile: (null)");
+                }
+            }
+        }        
+    }  
+
+    private void FindReplaceableTilesInTilemap(Tilemap tilemap)
+    {
+        foreach (var position in tilemap.cellBounds.allPositionsWithin)
+        {
+            TileBase tile = tilemap.GetTile(position);
+            if (tile != null)
+            {
+                // Export
+                Debug.Log("Export: " + tile.name);
+            }
+        }
+    }
+}
---
+++ WISM/Assets/Scripts/Tiles/VolcanoTile.cs
@@ -0,0 +1,30 @@
+﻿using Assets.Scripts;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class VolcanoTile : Tile
+{       
+    public override void RefreshTile(Vector3Int position, ITilemap tilemap)
+    {
+        HasTile hasTile = MountainTile.HasTile;
+        TileUtility.RefreshTile(position, tilemap, hasTile);
+    }
+
+#if UNITY_EDITOR
+    // Add tile type into Unity Editor
+
+    [MenuItem("Assets/Create/Tiles/VolcanoTile")]
+    public static void CreateVolcanoTile()
+    {
+        string path = EditorUtility.SaveFilePanelInProject("Save Volcano Tile", "New Volcano Tile", "asset", "Assets");
+        if (string.IsNullOrEmpty(path))
+            return;
+
+        AssetDatabase.CreateAsset(ScriptableObject.CreateInstance<VolcanoTile>(), path);
+    }
+
+#endif
+}
