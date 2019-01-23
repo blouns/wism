45c305563d1b63b2a72bc1fe96bc8c061c5a551e
blouns <blouns@outlook.com>
2019-01-22 23:44:20 -0800

    Simple terrain tilemap enumeration prototype

--- WISM/Assets/Scripts/Tiles/TerrainTilemap.cs
+++ WISM/Assets/Scripts/Tiles/TerrainTilemap.cs
@@ -3,48 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
-public class TilemapManager : MonoBehaviour
-{
-    static bool hasRun = false;
-
-    void Update()
+public class TerrainTilemap : MonoBehaviour
+{    
+    void Start()
     {
-        if (!hasRun)
-        {
-            hasRun = true;
-        }
-        else
-        {
-            return;
-        }
-            
+        int maxSize = 10000;
         Tilemap tilemap = GetComponent<Tilemap>();
-
+        tilemap.size = new Vector3Int(Mathf.Min(tilemap.size.x, maxSize), Mathf.Min(tilemap.size.y, maxSize), 1);
+        tilemap.ResizeBounds();
+        tilemap.CompressBounds();
         BoundsInt bounds = tilemap.cellBounds;
         TileBase[] allTiles = tilemap.GetTilesBlock(bounds);
-
         for (int x = 0; x < bounds.size.x; x++) {
-            for (int y = 0; y < bounds.size.y; y++) {
+            for (int y = 0; y < bounds.size.y; y++) {                
                 TileBase tile = allTiles[x + y * bounds.size.x];
                 if (tile != null) {
                     Debug.Log("x:" + x + " y:" + y + " tile:" + tile.name);
+                    
                 } else {
                     Debug.Log("x:" + x + " y:" + y + " tile: (null)");
+                }
+
+                if (maxSize-- < 0)
+                {
+                    Debug.Log("Broke out of large tilemap enumeration.");
+                    return;
                 }
             }
         }        
     }  
 
-    private void FindReplaceableTilesInTilemap(Tilemap tilemap)
-    {
-        foreach (var position in tilemap.cellBounds.allPositionsWithin)
-        {
-            TileBase tile = tilemap.GetTile(position);
-            if (tile != null)
-            {
-                // Export
-                Debug.Log("Export: " + tile.name);
-            }
-        }
-    }
 }
