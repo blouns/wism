b7f7e3c385b0d7f7dd532fb61d87a65d93aa904b
blouns <blouns@outlook.com>
2019-02-03 08:28:59 -0800

    Prototype: building the world from unity map; drawing hero based on cwism.

---
+++ WISM/Assets/Scripts/Units/ArmyGameObject.cs
@@ -0,0 +1,32 @@
+﻿using BranallyGames.Wism;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmyGameObject : MonoBehaviour
+{
+    public Army army;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (army != null)
+        {
+            if (army.Affiliation.IsHuman)
+            {
+                if (Input.GetKeyDown(KeyCode.DownArrow))
+                {
+                    bool moved = army.TryMove(Direction.South);
+                    if (moved)
+                    {
+                        Debug.Log(army.DisplayName + ": Moved south");
+                    }
+                    else
+                    {
+                        Debug.Log(army.DisplayName + ": Moved blocked");
+                    }
+                }
+            }
+        }
+    }
+}
